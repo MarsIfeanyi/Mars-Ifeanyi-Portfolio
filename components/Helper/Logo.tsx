a9b5d32c8c5ab29ad3571a6ea3a419c5a90c7d4a
MarsIfeanyi <marschalice@gmail.com>
2026-08-31 15:30:53 +0100

    Change Logo

--- components/Helper/Logo.tsx
+++ components/Helper/Logo.tsx
@@ -7,23 +7,34 @@ import React from 'react'
 const Logo = () => {
   return (
     <div className='flex items-center space-x-2' >
-<div className='border-2 border-blue-800 dark:border-blue-400 w-10 h-10 rounded-lg flex items-center justify-center flex-col' >
 
-
-
+<h1 className='shadow-2xl rounded-b-full' >
 <Image
-  src="/images/rocket.png"
+  src="/images/mars.jpg"
+  alt="Rocket"
+  width={80}
+  height={80}
+  className='rounded-full' 
+/>
+
+</h1>
+
+{/* <div className='border-2 border-blue-800 dark:border-blue-400 w-10 h-10 rounded-lg flex items-center justify-center flex-col' >
+<Image
+  src="/images/mars.jpg"
   alt="Rocket"
   width={24}
   height={24}
+  rounded-full
 />
-
-
 </div>
 <h1 className='sm:text-xl hidden sm:block md:text-2xl text-blue-800 dark:text-blue-400 font-bold' >
   
     {"Mars"}
-</h1>
+</h1> */}
+
+
+
 
     </div>
   )

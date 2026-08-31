952b66d3a74387e9e8d16db27b297d5d4eed3c56
MarsIfeanyi <marschalice@gmail.com>
2026-08-31 11:41:41 +0100

    Change Logo and background

--- components/Helper/Logo.tsx
+++ components/Helper/Logo.tsx
@@ -1,18 +1,27 @@
 import {  CodeSquareIcon, Mars, Rocket, Space } from 'lucide-react'
+import Image from 'next/image'
 import React from 'react'
+
+
 
 const Logo = () => {
   return (
     <div className='flex items-center space-x-2' >
-<div className='bg-blue-800 dark:bg-blue-400 w-10 h-10 rounded-lg flex items-center justify-center flex-col' >
+<div className='border-2 border-blue-800 dark:border-blue-400 w-10 h-10 rounded-lg flex items-center justify-center flex-col' >
 
 
 
-<Rocket className='text-white w-6 h-6'  />
+<Image
+  src="/images/rocket.png"
+  alt="Rocket"
+  width={24}
+  height={24}
+/>
 
 
 </div>
 <h1 className='sm:text-xl hidden sm:block md:text-2xl text-blue-800 dark:text-blue-400 font-bold' >
+  
     {"Mars"}
 </h1>
 

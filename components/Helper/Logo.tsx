0f1cdc01a294303684bb9b1ed95a84884a59cf6a
MarsIfeanyi <marschalice@gmail.com>
2026-09-01 17:00:40 +0100

    Fix style and href

--- components/Helper/Logo.tsx
+++ components/Helper/Logo.tsx
@@ -1,43 +1,20 @@
-import {  CodeSquareIcon, Mars, Rocket, Space } from 'lucide-react'
-import Image from 'next/image'
-import React from 'react'
-
-
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Logo = () => {
   return (
-    <div className='flex items-center space-x-2' >
+    <Link href="/#home" aria-label="Go to homepage">
+      <Image
+        src="/images/mars.jpg"
+        alt="Mars Ifeanyi Logo"
+        width={80}
+        height={80}
+        priority
+        className="rounded-full shadow-2xl cursor-pointer"
+      />
+    </Link>
+  );
+};
 
-<h1 className='shadow-2xl rounded-b-full' >
-<Image
-  src="/images/mars.jpg"
-  alt="LogoImage"
-  width={80}
-  height={80}
-  className='rounded-full' 
-/>
-
-</h1>
-
-{/* <div className='border-2 border-blue-800 dark:border-blue-400 w-10 h-10 rounded-lg flex items-center justify-center flex-col' >
-<Image
-  src="/images/mars.jpg"
-  alt="Rocket"
-  width={24}
-  height={24}
-  rounded-full
-/>
-</div>
-<h1 className='sm:text-xl hidden sm:block md:text-2xl text-blue-800 dark:text-blue-400 font-bold' >
-  
-    {"Mars"}
-</h1> */}
-
-
-
-
-    </div>
-  )
-}
-
-export default Logo
+export default Logo;

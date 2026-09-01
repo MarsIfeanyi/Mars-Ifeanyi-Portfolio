75fadf1f78ed2cfd5f0c4fa296f998cc8e8e188e
MarsIfeanyi <marschalice@gmail.com>
2026-09-01 16:59:00 +0100

    Add Smooth Scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,32 +3,30 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Provider from "@/components/Hoc/Provider";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
+import Footer from "@/components/Home/Footer/Footer";
+import ScrollToTop from "@/components/Helper/ScrollToTop";
 
 const font = Inter({
-  weight:['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  subsets:['latin']
-
-})
-
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Marcellus Ifeanyi Ugokwe",
-  description: "Marcellus Ifeanyi Ugokwe, Data Engineer | Software Engineer | Data Analytics Engineer | Quant & Chartered Trader | AI/ML Engineer | Full-Stack Blockchain Developer",
+  description:
+    "Marcellus Ifeanyi Ugokwe, Data Engineer | Software Engineer | Data Analytics Engineer | Quant & Chartered Trader | AI/ML Engineer | Full-Stack Blockchain Developer",
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html
-      lang="en" suppressHydrationWarning
-    >
-      <body  className={`${font.className} antialiased`} >
-
-
+    <html lang="en" suppressHydrationWarning className="scroll-smooth">
+      <body className={`${font.className} antialiased`}>
         <Provider>
-          <ResponsiveNav/>
-          {children}   
+          <ResponsiveNav />
+          {children}
+          <Footer />
+          <ScrollToTop />
         </Provider>
-
       </body>
     </html>
   );

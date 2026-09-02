7b91fc6d90420a1ebd51222b2e7531d1ed99aa54
MarsIfeanyi <marschalice@gmail.com>
2026-09-02 13:43:33 +0100

    Create and Render the Skills Page

---
+++ app/skills/page.tsx
@@ -0,0 +1,15 @@
+import PageContainer from "@/components/Helper/PageContainer";
+import Skills from "@/components/Home/Skills/Skills";
+import React from "react";
+
+const SkillsPage = () => {
+  return (
+    <div>
+      <PageContainer>
+        <Skills />
+      </PageContainer>
+    </div>
+  );
+};
+
+export default SkillsPage;

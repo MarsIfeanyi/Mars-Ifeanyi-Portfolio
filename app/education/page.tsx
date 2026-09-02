e2806d7cffcb5b44c16fb7c13cba454221c80b9b
MarsIfeanyi <marschalice@gmail.com>
2026-09-02 13:42:31 +0100

    Create and Render the Education Page

---
+++ app/education/page.tsx
@@ -0,0 +1,15 @@
+import PageContainer from "@/components/Helper/PageContainer";
+import Education from "@/components/Home/Education/Education";
+import React from "react";
+
+const EducationPage = () => {
+  return (
+    <div>
+      <PageContainer>
+        <Education />
+      </PageContainer>
+    </div>
+  );
+};
+
+export default EducationPage;

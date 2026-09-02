782117bd70336f429e04ac50af1c0cef13cb6f86
MarsIfeanyi <marschalice@gmail.com>
2026-09-02 13:42:01 +0100

    Create and Render the About Page

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import About from "@/components/Home/About/About";
+import PageContainer from "@/components/Helper/PageContainer";
+
+const AboutPage = () => {
+  return (
+    <PageContainer>
+      <About />
+    </PageContainer>
+  );
+};
+
+export default AboutPage;

49ad22e5291733f3a6de58a77b1c34cb70deae6c
MarsIfeanyi <marschalice@gmail.com>
2026-09-02 11:57:17 +0100

    Add Education Navlink

--- constants/constant.ts
+++ constants/constant.ts
@@ -1,9 +1,10 @@
 export const Navlinks = [
-  { name: "Home", href: "" },
+  // { name: "Home", href: "" },
   { name: "About", href: "/#about" },
   { name: "Skills", href: "/#skills" },
   { name: "Projects", href: "/#projects" },
   { name: "Experience", href: "/#experience" },
+  { name: "Education", href: "#education" },
   // { name: "Testimonials", href: "/#testimonials" },
   { name: "Contact", href: "/#contact" },
 ];

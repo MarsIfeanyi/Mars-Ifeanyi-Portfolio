8c879902272c54c05b86146c05ceb30464909898
MarsIfeanyi <marschalice@gmail.com>
2026-09-02 13:43:08 +0100

    Create and Render the Project Page

---
+++ app/projects/page.tsx
@@ -0,0 +1,15 @@
+import PageContainer from "@/components/Helper/PageContainer";
+import Project from "@/components/Home/Project/Project";
+import React from "react";
+
+const ProjectsPage = () => {
+  return (
+    <div>
+      <PageContainer>
+        <Project />
+      </PageContainer>
+    </div>
+  );
+};
+
+export default ProjectsPage;

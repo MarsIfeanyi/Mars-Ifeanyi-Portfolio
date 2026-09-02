da63aca822e932a1db110add7085aba283af6b35
MarsIfeanyi <marschalice@gmail.com>
2026-09-02 13:42:50 +0100

    Create and Render the Eexpereince Page

---
+++ app/experience/page.tsx
@@ -0,0 +1,15 @@
+import PageContainer from "@/components/Helper/PageContainer";
+import Experience from "@/components/Home/Experience/Experience";
+import React from "react";
+
+const ExperiencePage = () => {
+  return (
+    <div>
+      <PageContainer>
+        <Experience />
+      </PageContainer>
+    </div>
+  );
+};
+
+export default ExperiencePage;

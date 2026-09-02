853fb8ac44db893b6aa42d5cc944b12fa695ca3d
MarsIfeanyi <marschalice@gmail.com>
2026-09-02 13:42:15 +0100

    Create and Render the Contact Page

---
+++ app/contact/page.tsx
@@ -0,0 +1,15 @@
+import PageContainer from "@/components/Helper/PageContainer";
+import Contact from "@/components/Home/Contact/Contact";
+import React from "react";
+
+const ContactPage = () => {
+  return (
+    <div>
+      <PageContainer>
+        <Contact />
+      </PageContainer>
+    </div>
+  );
+};
+
+export default ContactPage;

86b1f8bc3b9cc682f405f0eecedbd3fb5dc84ac7
MarsIfeanyi <marschalice@gmail.com>
2026-09-02 13:41:30 +0100

    Update file

--- constants/constant.ts
+++ constants/constant.ts
@@ -1,10 +1,30 @@
 export const Navlinks = [
-  // { name: "Home", href: "" },
-  { name: "About", href: "/#about" },
-  { name: "Skills", href: "/#skills" },
-  { name: "Projects", href: "/#projects" },
-  { name: "Experience", href: "/#experience" },
-  { name: "Education", href: "#education" },
-  // { name: "Testimonials", href: "/#testimonials" },
-  { name: "Contact", href: "/#contact" },
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "About",
+    href: "/about",
+  },
+  {
+    name: "Skills",
+    href: "/skills",
+  },
+  {
+    name: "Projects",
+    href: "/projects",
+  },
+  {
+    name: "Experience",
+    href: "/experience",
+  },
+  {
+    name: "Education",
+    href: "/education",
+  },
+  {
+    name: "Contact",
+    href: "/contact",
+  },
 ];
